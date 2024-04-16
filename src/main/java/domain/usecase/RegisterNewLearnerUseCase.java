e636d9e509479542542e0b04f7cc4ffcead3bfd9
tegajoel <oe23abh@herts.ac.uk>
2024-04-16 19:24:04 +0100

    Added comment for useCase execution

--- src/main/java/domain/usecase/RegisterNewLearnerUseCase.java
+++ src/main/java/domain/usecase/RegisterNewLearnerUseCase.java
@@ -12,27 +12,34 @@ public class RegisterNewLearnerUseCase {
         this.repository = repository;
     }
 
-    public Result<Object, Error> execute(Learner learner){
-        if (learner.getAge() < 4 || learner.getAge() > 11){
+    /**
+     * Register a new learner
+     *
+     * @param learner learner to be registered
+     * @return Result with {@link Result#NO_VALUE} on success,
+     * otherwise, {@link Result#error(Object)} with {@link Error} as its error data
+     */
+    public Result<Object, Error> execute(Learner learner) {
+        if (learner.getAge() < 4 || learner.getAge() > 11) {
             return Result.error(Error.INVALID_AGE);
         }
 
-        if (learner.getGrade() < 0 || learner.getGrade() > 5){
+        if (learner.getGrade() < 0 || learner.getGrade() > 5) {
             return Result.error(Error.INVALID_GRADE);
         }
 
-        if (!validatePhoneNumber(learner.getPhoneNumber()) || !validatePhoneNumber(learner.getEmergencyContactNumber())){
+        if (!validatePhoneNumber(learner.getPhoneNumber()) || !validatePhoneNumber(learner.getEmergencyContactNumber())) {
             return Result.error(Error.INVALID_PHONE_NUMBER);
         }
 
-        if (repository.addNewLearner(learner.setId(IdGenerator.generateId(learner)))){
+        if (repository.addNewLearner(learner.setId(IdGenerator.generateId(learner)))) {
             return Result.success(Result.NO_VALUE);
         } else {
             return Result.error(Error.REPOSITORY_ERROR);
         }
     }
 
-    private boolean validatePhoneNumber(String phoneNumber){
+    private boolean validatePhoneNumber(String phoneNumber) {
         if (phoneNumber == null || phoneNumber.isBlank()) return false;
 
         if (phoneNumber.length() != 11) return false;

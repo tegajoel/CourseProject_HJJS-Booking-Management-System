fc848678dbf0d4e3fb3e647c737eedfe7252d8ae
tegajoel <oe23abh@herts.ac.uk>
2024-04-16 23:20:00 +0100

    Implemented CancelLessonUseCase and  wrote tests for it

---
+++ src/main/java/domain/usecase/CancelLessonUseCase.java
@@ -0,0 +1,33 @@
+package domain.usecase;
+
+import domain.entity.Learner;
+import domain.entity.lesson.Lesson;
+import domain.entity.lesson.LessonStatus;
+import domain.util.Result;
+
+public class CancelLessonUseCase {
+
+    /**
+     * Cancel the booked lesson of this learner
+     * @param lessonToCancel lesson to cancel
+     * @param learner the learner
+     */
+    public Result<Object, Error> execute(Lesson lessonToCancel, Learner learner){
+        if (!learner.hasLessonRegistered(lessonToCancel)){
+            return Result.error(Error.NO_BOOKING_FOR_LESSON);
+        }
+
+        return switch (learner.getLessonStatus(lessonToCancel)){
+            case BOOKED -> {
+                learner.updateRegisteredLessonStatus(lessonToCancel, LessonStatus.CANCELLED);
+                yield Result.success(Result.NO_VALUE);
+            }
+            case ATTENDED -> Result.error(Error.LESSON_ALREADY_ATTENDED);
+            case CANCELLED -> Result.error(Error.LESSON_ALREADY_CANCELLED);
+        };
+    }
+
+    public enum Error{
+        LESSON_ALREADY_ATTENDED, LESSON_ALREADY_CANCELLED, NO_BOOKING_FOR_LESSON
+    }
+}
---
+++ src/test/java/domain/usecase/CancelLessonUseCaseTest.java
@@ -0,0 +1,85 @@
+package domain.usecase;
+
+import domain.entity.Coach;
+import domain.entity.Learner;
+import domain.entity.lesson.Lesson;
+import domain.entity.lesson.LessonStatus;
+import domain.entity.lesson.RegisteredLesson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CancelLessonUseCaseTest {
+    private CancelLessonUseCase sut;
+    private Learner learner;
+    private Lesson testLesson;
+    private final String validPhoneNumber = "08012345680";
+    private final int passingTestGrade = 3;
+
+    @BeforeEach
+    void setup() {
+        sut = new CancelLessonUseCase();
+        learner = new Learner("John doe", "Male", 5, passingTestGrade, validPhoneNumber, validPhoneNumber);
+        Coach coach = new Coach("Peter");
+        testLesson = new Lesson("Diving", passingTestGrade, coach, LocalDate.now());
+        coach.assignLesson(testLesson);
+    }
+
+    @Test
+    public void cancelLesson_registeredAndBookedLesson_Succeeds() {
+        learner.registerNewLesson(new RegisteredLesson(testLesson, LessonStatus.BOOKED));
+
+        assertTrue(sut.execute(testLesson, learner).isSuccess());
+    }
+
+    @Test
+    public void cancelLesson_succeeds_lessonStatusUpdatedToCancelled() {
+        learner.registerNewLesson(new RegisteredLesson(testLesson, LessonStatus.BOOKED));
+
+        sut.execute(testLesson, learner);
+        assertEquals(LessonStatus.CANCELLED, learner.getLessonStatus(testLesson));
+    }
+
+    @Test
+    public void cancelLesson_registeredAndCancelledLesson_fails() {
+        learner.registerNewLesson(new RegisteredLesson(testLesson, LessonStatus.CANCELLED));
+
+        assertFalse(sut.execute(testLesson, learner).isSuccess());
+    }
+
+    @Test
+    public void cancelLesson_registeredAndAttendedLesson_fails() {
+        learner.registerNewLesson(new RegisteredLesson(testLesson, LessonStatus.ATTENDED));
+
+        assertFalse(sut.execute(testLesson, learner).isSuccess());
+    }
+
+    @Test
+    public void cancelLesson_notRegistered_fails() {
+        assertFalse(sut.execute(testLesson, learner).isSuccess());
+    }
+
+    @Test
+    public void cancelLesson_notRegistered_failsWithCorrectError() {
+        assertEquals(CancelLessonUseCase.Error.NO_BOOKING_FOR_LESSON, sut.execute(testLesson, learner).getError());
+    }
+
+    @Test
+    public void cancelLesson_registeredAndCancelledLesson_failsWithCorrectError() {
+        learner.registerNewLesson(new RegisteredLesson(testLesson, LessonStatus.CANCELLED));
+
+        assertEquals(CancelLessonUseCase.Error.LESSON_ALREADY_CANCELLED, sut.execute(testLesson, learner).getError());
+    }
+
+    @Test
+    public void cancelLesson_registeredAndAttendedLesson_failsWithCorrectError() {
+        learner.registerNewLesson(new RegisteredLesson(testLesson, LessonStatus.ATTENDED));
+
+        assertEquals(CancelLessonUseCase.Error.LESSON_ALREADY_ATTENDED, sut.execute(testLesson, learner).getError());
+    }
+
+
+}

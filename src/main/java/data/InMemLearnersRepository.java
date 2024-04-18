d39873d0a17e981c3af8e54df5c3af22aacec808
tegajoel <oe23abh@herts.ac.uk>
2024-04-18 13:44:14 +0100

    Implemented an in memory learners repository

---
+++ src/main/java/data/InMemLearnersRepository.java
@@ -0,0 +1,34 @@
+package data;
+
+import domain.entity.Learner;
+import domain.repository.LearnerRepository;
+import domain.util.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemLearnersRepository implements LearnerRepository {
+    private final List<Learner> learners = new ArrayList<>();
+    @Override
+    public boolean addNewLearner(Learner learner) {
+        return learners.add(learner);
+    }
+
+    @Override
+    public List<Learner> getAllLearners() {
+        return learners;
+    }
+
+    @Override
+    public Result<Learner, LearnerRepository.Error> getLearnerById(int id) {
+        for (Learner learner : learners) {
+            if (learner.getId() == id) return Result.success(learner);
+        }
+
+        return Result.error(Error.LEARNER_NOT_FOUND);
+    }
+
+    public void addLearners(List<Learner> learners){
+        this.learners.addAll(learners);
+    }
+}

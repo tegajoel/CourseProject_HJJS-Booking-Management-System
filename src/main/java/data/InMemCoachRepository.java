06956ca1d95adb35ca90b169e98e0870a21bf4e6
tegajoel <oe23abh@herts.ac.uk>
2024-04-18 13:46:42 +0100

    Implemented an in memory coach repository

---
+++ src/main/java/data/InMemCoachRepository.java
@@ -0,0 +1,24 @@
+package data;
+
+import domain.entity.Coach;
+import domain.repository.CoachRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemCoachRepository implements CoachRepository {
+    private final List<Coach> coaches = new ArrayList<>();
+    @Override
+    public List<Coach> getAllCoaches() {
+        return coaches;
+    }
+
+    @Override
+    public void addNewCoach(Coach coach) {
+        coaches.add(coach);
+    }
+
+    public void addCoaches(List<Coach> coaches){
+        this.coaches.addAll(coaches);
+    }
+}

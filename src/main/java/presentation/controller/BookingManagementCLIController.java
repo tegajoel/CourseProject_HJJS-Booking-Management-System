3f5fb039b4016b1964a533019e294f0b19a0fb39
tegajoel <oe23abh@herts.ac.uk>
2024-04-19 13:34:53 +0100

    Implemented booking and attending a lesson

--- src/main/java/presentation/controller/BookingManagementCLIController.java
+++ src/main/java/presentation/controller/BookingManagementCLIController.java
@@ -1,6 +1,14 @@
 package presentation.controller;
 
+import data.InMemLearnersRepository;
+import data.InMemLessonRepository;
+import domain.entity.Review;
+import domain.entity.coach.Coach;
 import domain.entity.learner.Learner;
+import domain.entity.lesson.Lesson;
+import domain.entity.lesson.LessonStatus;
+import domain.entity.lesson.RegisteredLesson;
+import domain.repository.CoachRepository;
 import domain.repository.LearnerRepository;
 import domain.usecase.*;
 import domain.util.Result;
@@ -10,7 +18,9 @@ import presentation.view.ReportPrinter;
 import presentation.view.widgets.optionpicker.OptionPickerStyle;
 import presentation.view.widgets.text.MessageType;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class BookingManagementCLIController {
     private BookingManagementCLIView view;
@@ -23,6 +33,7 @@ public class BookingManagementCLIController {
     private final RegisterNewLearnerUseCase registerNewLearnerUseCase;
 
     private final LearnerRepository learnerRepository;
+    private final CoachRepository coachRepository;
 
     String nameBeingRegistered = null;
     String genderBeingRegistered = null;
@@ -39,7 +50,8 @@ public class BookingManagementCLIController {
                                           GenerateCoachReportUseCase generateCoachReportUseCase,
                                           GenerateLearnerReportUseCase generateLearnerReportUseCase,
                                           RegisterNewLearnerUseCase registerNewLearnerUseCase,
-                                          LearnerRepository learnerRepository) {
+                                          LearnerRepository learnerRepository,
+                                          CoachRepository coachRepository) {
         this.view = view;
         this.attendLessonUseCase = attendLessonUseCase;
         this.bookLessonUseCase = bookLessonUseCase;
@@ -49,6 +61,7 @@ public class BookingManagementCLIController {
         this.generateLearnerReportUseCase = generateLearnerReportUseCase;
         this.registerNewLearnerUseCase = registerNewLearnerUseCase;
         this.learnerRepository = learnerRepository;
+        this.coachRepository = coachRepository;
 
         initialize();
     }
@@ -56,14 +69,40 @@ public class BookingManagementCLIController {
 
     private void initialize() {
         view.displayAppHeader("Booking Management System");
+        Coach coach1 = new Coach("Peter");
+        Coach coach2 = new Coach("John");
+        coachRepository.addNewCoach(coach1);
+        coachRepository.addNewCoach(coach2);
+
+        Lesson lesson = new Lesson("Diving 1", 3, coach1, LocalDate.now());
+        Lesson lesson2 = new Lesson("Diving 2", 3, coach1, LocalDate.now());
+        Lesson lesson3 = new Lesson("Diving 3", 4, coach2, LocalDate.now());
+        Lesson lesson4 = new Lesson("Diving 4", 2, coach1, LocalDate.now());
+
+        Learner learner = new Learner("Paul", "Male", 6, 3, "0819ja", "0819ja",
+                List.of(new RegisteredLesson(lesson, LessonStatus.BOOKED), new RegisteredLesson(lesson2, LessonStatus.ATTENDED))).setId(1234);
+
+        Learner learner1 = new Learner("Simon", "Male", 6, 3, "0819ja", "0819ja",
+                List.of(new RegisteredLesson(lesson, LessonStatus.BOOKED), new RegisteredLesson(lesson3, LessonStatus.ATTENDED))).setId(123);
+
+        InMemLearnersRepository.getInstance().addNewLearner(learner1);
+        InMemLearnersRepository.getInstance().addNewLearner(learner);
+
+        lesson3.addLearner(learner1);
+        lesson.addLearner(learner1);
+        lesson.addLearner(learner);
+        lesson2.addLearner(learner);
+
+        InMemLessonRepository.getInstance().addLessons(List.of(lesson2, lesson2, lesson3, lesson4, lesson));
 
         showMainMenuOptions();
     }
 
     private void showMainMenuOptions() {
         List<String> menuOptions = List.of(
-                "Register new Learner",
-                "Manage/Cancel Booking",
+                "Register a new Learner",
+                "Book a swimming lesson",
+                "Manage/Cancel a Booking",
                 "Attend a swimming lesson",
                 "Print Learner Report",
                 "Print Coach Report",
@@ -73,13 +112,14 @@ public class BookingManagementCLIController {
         view.showOptionsPicker(menuOptions, OptionPickerStyle.VERTICAL, "Main menu", (index, value) -> {
             switch (index) {
                 case 0 -> {
-                    view.displayMessage("We'll need some details to get you registered\n", MessageType.INFO);
+                    view.displayMessage("We'll need some details to get you registered", MessageType.INFO);
                     onRegisterNewUser();
                 }
-                case 1 -> onManageBooking();
-                case 2 -> onAttendSwimmingLesson();
-                case 3 -> onPrintLearnerReport();
-                case 4 -> onPrintCoachReport();
+                case 1 -> onBookSwimmingLesson();
+                case 2 -> onManageBooking();
+                case 3 -> onAttendSwimmingLesson();
+                case 4 -> onPrintLearnerReport();
+                case 5 -> onPrintCoachReport();
                 default -> {
                     view.displayMessage("Invalid Selection", MessageType.ERROR);
                     showMainMenuOptions();
@@ -88,10 +128,34 @@ public class BookingManagementCLIController {
         });
     }
 
+    private void onBookSwimmingLesson() {
+        requestLearnerById(learner -> {
+            requestLessonFromLessons(lesson -> {
+                var result = bookLessonUseCase.bookLesson(lesson, learner);
+
+                if (result.isSuccess()) {
+                    printBookingDetails(learner, lesson);
+                    showExitOrMainMenuOption();
+                } else {
+                    String errorMsg = switch (result.getError()){
+                        case LESSON_ABOVE_LEARNER_GRADE -> "This grade (" + lesson.getGrade()+ ") lesson is above your current grade of " + learner.getGrade();
+                        case LESSON_BELOW_LEARNER_GRADE -> "This grade (" + lesson.getGrade()+ ") lesson is below your current grade of " + learner.getGrade();
+                        case DUPLICATE_BOOKING -> "You have already booked this lesson";
+                        case LESSON_FULLY_BOOKED -> "This lesson is fully booked";
+                    };
+                    view.displayMessage("Booking Failed", MessageType.ERROR);
+                    view.displayMessage(errorMsg, MessageType.ERROR);
+
+                    showExitOrMainMenuOption();
+                }
+            });
+        });
+    }
+
     private void showExitOrMainMenuOption() {
         var options = List.of("Return to main menu", "Exit App");
         view.showOptionsPicker(options, OptionPickerStyle.HORIZONTAL, null, (index, value) -> {
-            if (index == 0){
+            if (index == 0) {
                 showMainMenuOptions();
             } else {
                 closeApp();
@@ -99,7 +163,7 @@ public class BookingManagementCLIController {
         });
     }
 
-    private void closeApp(){
+    private void closeApp() {
         view.displayMessage("Exiting...", MessageType.INFO);
         System.exit(0);
     }
@@ -200,9 +264,218 @@ public class BookingManagementCLIController {
         showMainMenuOptions();
     }
 
-    private void onAttendSwimmingLesson(){
+    private void onAttendSwimmingLesson() {
+        requestLearnerById(learner -> {
+
+            List<Lesson> bookedLessons = learner.getRegisteredLessons().stream()
+                    .filter(lesson -> lesson.getLessonStatus() == LessonStatus.BOOKED)
+                    .map(RegisteredLesson::getLesson)
+                    .toList();
+
+            if (bookedLessons.isEmpty()){
+                view.displayMessage("You don't have any booked lesson to attend", MessageType.ERROR);
+                var options = List.of("Book a lesson", "Return to Main Menu", "Exit App");
+                view.showOptionsPicker(options, OptionPickerStyle.HORIZONTAL, "Choose an option", (index, value) -> {
+                    switch (index){
+                        case 0 -> onBookSwimmingLesson();
+                        case 1 -> showMainMenuOptions();
+                        case 2 -> closeApp();
+                    }
+                });
+            } else {
+                requestPickFromLessons("Please select from one of your booked lesson to attend", bookedLessons, lesson -> {
+                    var result = attendLessonUseCase.attendLesson(lesson, learner, this::provideLessonReview);
+
+                    if (result.isSuccess()) {
+                        view.displayMessage("Lesson attended!", MessageType.INFO);
+                        view.displayMessage(getLearnerDetails(learner, true), MessageType.INFO);
+                        showExitOrMainMenuOption();
+                    } else {
+                        String errorMsg = switch (result.getError()){
+                            case LEARNER_NOT_REGISTERED_TO_LESSON -> "You cannot attend this lesson as you are not registered for it";
+                            case LESSON_ALREADY_ATTENDED -> "You have already attended this lesson";
+                            case INVALID_REVIEW_RATING -> "Review rating invalid. it should be between 1 to 5";
+                            case EMPTY_REVIEW_MESSAGE -> "You provided an empty review message";
+                        };
+                        view.displayMessage(errorMsg, MessageType.ERROR);
+                        showExitOrMainMenuOption();
+                    }
+                });
+            }
+        });
+    }
+
+    private void requestLearnerById(Consumer<Learner> consumer) {
+        view.requestUserInput("Please enter a learner ID", data -> {
+            if (data.trim().equals("0")) {
+                showMainMenuOptions();
+                return InputConsumer.success;
+            }
+
+            try {
+                int id = Integer.parseInt(data);
+                var result = learnerRepository.getLearnerById(id);
+                if (result.isSuccess()) {
+                    consumer.accept(result.getData());
+                    return InputConsumer.success;
+                } else {
+                    view.displayMessage("\nEnter 0 to to return to the Main Menu", MessageType.INFO);
+                    return Result.error("No learner exits with this id.");
+                }
+            } catch (Exception e) {
+                return Result.error("Learner IDs only contain numbers");
+            }
+        });
+    }
+
+    private void requestPickFromLessons(String message, List<Lesson> lessons, Consumer<Lesson> lessonConsumer){
+        List<String> options = lessons.stream().map(this::getLessonDetails).toList();
+        view.showOptionsPicker(options, OptionPickerStyle.VERTICAL, message, (index, value) -> {
+            lessonConsumer.accept(lessons.get(index));
+        });
+    }
+
+    private void requestLessonFromLessons(Consumer<Lesson> lessonConsumer) {
+        showOptionToPickLesson(lessons -> {
+            if (lessons.isEmpty()) {
+                view.displayMessage("There are no lesson to choose from", MessageType.INFO);
+                showExitOrMainMenuOption();
+                return;
+            }
+
+            List<String> options = lessons.stream().map(this::getLessonDetails).toList();
+            view.showOptionsPicker(options, OptionPickerStyle.VERTICAL, null, (index, value) -> {
+                lessonConsumer.accept(lessons.get(index));
+            });
+        });
+    }
+
+    private void showOptionToPickLesson(Consumer<List<Lesson>> caller) {
+        var options = List.of("By Swimming Grade", "By Coach", "By Day");
+        view.showOptionsPicker(
+                options,
+                OptionPickerStyle.HORIZONTAL,
+                "How would you like to view the available lessons?",
+                (index, value) -> {
+                    switch (index) {
+                        case 0 -> onFilterLessonByGrade(caller);
+                        case 1 -> onFilterLessonByCoach(caller);
+                        case 2 -> onFilterLessonByDay(caller);
+                    }
+                });
+    }
+
+    private void onFilterLessonByGrade(Consumer<List<Lesson>> caller) {
+        view.requestUserInput("Please enter a grade level [1-5]", data -> {
+            try {
+                int grade = Integer.parseInt(data);
+                var result = filterLessonsUseCase.filterByGrade(grade);
+
+                if (result.isSuccess()) {
+                    caller.accept(result.getData());
+                    return InputConsumer.success;
+                } else {
+                    return Result.error("Grade is invalid");
+                }
+            } catch (Exception e) {
+                return Result.error("Grade must be a numeric value");
+            }
+        });
+    }
+
+    private void onFilterLessonByDay(Consumer<List<Lesson>> caller) {
+        view.requestUserInput("Please a week day e.g Wednesday", data -> {
+            var result = filterLessonsUseCase.filterByDay(data);
+            if (result.isSuccess()) {
+                caller.accept(result.getData());
+                return InputConsumer.success;
+            } else {
+                return Result.error("Day is not valid");
+            }
+        });
+    }
+
+    private void onFilterLessonByCoach(Consumer<List<Lesson>> caller) {
+        view.requestUserInput("Please the name of the coach. 0 to show all coaches", data -> {
+            if (data.trim().equals("0")) {
+                getCoachFromAllCoaches(coach -> {
+                    var result = filterLessonsUseCase.filterByCoach(data);
+                    if (result.isSuccess()) {
+                        caller.accept(result.getData());
+                    } else {
+                        view.displayMessage("System Error", MessageType.ERROR);
+                        showMainMenuOptions();
+                    }
+                });
+                return InputConsumer.success;
+            }
+
+            var result = filterLessonsUseCase.filterByCoach(data);
+            if (result.isSuccess()) {
+                caller.accept(result.getData());
+                return InputConsumer.success;
+            } else {
+                return Result.error("There is no coach with the name: " + data);
+            }
+        });
+    }
+
+    private void getCoachFromAllCoaches(Consumer<Coach> consumer) {
+        List<Coach> coaches = coachRepository.getAllCoaches();
+        List<String> options = coaches.stream().map(this::getCoachDetails).toList();
+        view.showOptionsPicker(options, OptionPickerStyle.VERTICAL, null, (index, value) -> consumer.accept(coaches.get(index)));
+    }
+
+    private String getCoachDetails(Coach coach) {
+        // TODO: 4/18/2024 add good details
+        return coach.toString();
+    }
+
+    private String getLessonDetails(Lesson lesson) {
+        // TODO: 4/18/2024 add good details
+        return lesson.toString();
+    }
+
+    private String getLearnerDetails(Learner learner, boolean prettyPrint) {
+        // TODO: 4/18/2024 add good details
+        return learner.toString();
+    }
+
+    private void printBookingDetails(Learner learner, Lesson lesson){
+        // TODO: 4/19/2024
+        view.displayMessage("Lesson Booked!", MessageType.INFO);
+
+        view.displayMessage(getLearnerDetails(learner, true), MessageType.INFO);
+    }
+
+    private Review provideLessonReview() {
+        final boolean[] hasCompletedReview = {false};
+        Review review = new Review("", 0);
+        var options = List.of("Very dissatisfied", "Dissatisfied", "Ok", "Satisfied", "Very Satisfied");
+
+        view.displayMessage("Please leave a review for this lesson", MessageType.INFO);
+
+        view.requestUserInput("Review Message", data -> {
+            review.updateMessage(data);
+            view.showOptionsPicker(
+                    options,
+                    OptionPickerStyle.HORIZONTAL,
+                    "How would you rate this lesson?",
+                    (index, value) -> {
+                        review.updateRating(index + 1);
+                        hasCompletedReview[0] = true;
+                    });
+            return InputConsumer.success;
+        });
+
+        while (!hasCompletedReview[0]){
+            // delay until we have received the review
+        }
+
+        return review;
 
     }
+
 
     private void onManageBooking() {
 
@@ -215,32 +488,15 @@ public class BookingManagementCLIController {
     private void onPrintLearnerReport() {
         var options = List.of("Print for all learners", "Print for specific learners");
         view.showOptionsPicker(options, OptionPickerStyle.HORIZONTAL, null, (index, value) -> {
-            if (index == 0){
+            if (index == 0) {
                 String msg = ReportPrinter.prettyPrintLearnerReports(generateLearnerReportUseCase.getReportForAllLearners());
                 view.displayMessage(msg, MessageType.INFO);
                 showExitOrMainMenuOption();
             } else {
-                view.requestUserInput("Please provide the id of the learner", data -> {
-                    if (data.trim().equals("0")){
-                        showMainMenuOptions();
-                        return InputConsumer.success;
-                    }
-
-                    try {
-                        int id = Integer.parseInt(data);
-                        var result = learnerRepository.getLearnerById(id);
-                        if (result.isSuccess()){
-                            String msg = ReportPrinter.prettyPrintLearnerReport(generateLearnerReportUseCase.getReportForLearner(result.getData()));
-                            view.displayMessage(msg, MessageType.INFO);
-                            showExitOrMainMenuOption();
-                            return InputConsumer.success;
-                        } else {
-                            view.displayMessage("\nEnter 0 to to return to the Main Menu", MessageType.INFO);
-                            return Result.error("No learner exits with this id.");
-                        }
-                    } catch (Exception e){
-                        return Result.error("Learner IDs only contain numbers");
-                    }
+                requestLearnerById(learner -> {
+                    String msg = ReportPrinter.prettyPrintLearnerReport(generateLearnerReportUseCase.getReportForLearner(learner));
+                    view.displayMessage(msg, MessageType.INFO);
+                    showExitOrMainMenuOption();
                 });
             }
         });

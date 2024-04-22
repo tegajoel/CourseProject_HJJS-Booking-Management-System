257061f5dd10001544e1d697783d14c5040d9e06
tegajoel <oe23abh@herts.ac.uk>
2024-04-22 16:35:43 +0100

    Added a MainTestSuite to run all domain and presentation tests
    - Added testsuite dependencies to pom.xml

---
+++ src/test/java/MainTestSuite.java
@@ -0,0 +1,11 @@
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith(org.junit.platform.runner.JUnitPlatform.class)
+@SuiteDisplayName("JUnit Platform Suite Demo")
+@SelectPackages({"domain", "presentation"})
+public class MainTestSuite {
+
+}
